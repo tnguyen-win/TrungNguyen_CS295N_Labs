cf3cc1a2442e180e7d70e75a32cf172dba23534e
tnguyen-win <129979622+tnguyen-win@users.noreply.github.com>
2023-10-19 12:52:52 -0700

    Lab 4 - Final - v1

--- PopularGameEngines/Models/Message.cs
+++ PopularGameEngines/Models/Message.cs
@@ -1,7 +1,7 @@
 namespace PopularGameEngines.Models {
     public class Message {
         public string? Title { get; set; }
-        public string? Text { get; set; }
+        public string? Body { get; set; }
         public AppUser? From { get; set; }
         public DateOnly? Date { get; set; }
         public int? Rating { get; set; }
